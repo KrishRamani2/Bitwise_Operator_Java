c5b8cb7ad15aa85fe045a300a93586980e5b1666
Krish Vijay Ramani <136609165+KrishRamani2@users.noreply.github.com>
2023-07-31 21:25:40 +0530

    Add files via upload

---
+++ code (1).java
@@ -0,0 +1,28 @@
+// Bitwise Operator
+import java.util.*;
+import java.io.*;
+
+class GFG {
+	
+	/* Driver program to test above function */
+	public static void main(String[] args) throws IOException
+	{
+		int a=10 , b=14 ;
+		
+		// left shift  
+		System.out.println(a<<1);
+		System.out.println(a<<2);
+		System.out.println(a<<4);
+		
+		// right shift
+		System.out.println(a>>1);
+		System.out.println(a>>2);
+		System.out.println(a>>4);
+		System.out.println(x >>> 1)
+		
+		// negation Operator
+	    System.out.println(~a);
+		System.out.println(~b);
+	}
+}
+	
